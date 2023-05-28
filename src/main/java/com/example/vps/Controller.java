484aad5fd17142ccef260e400b2786a1c8a5dae1
Nilarnab <nilarnabdebnath@gmail.com>
2023-05-28 22:33:47 +0530

    added fetch from s3 bucket; loop complete

--- src/main/java/com/example/vps/Controller.java
+++ src/main/java/com/example/vps/Controller.java
@@ -3,6 +3,7 @@ package com.example.vps;
 import com.example.vps.utils.Response;
 import com.example.vps.utils.VideoProcessor;
 import io.minio.*;
+import io.minio.errors.*;
 import io.minio.messages.Item;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,11 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import io.minio.errors.MinioException;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -25,7 +26,7 @@ public class Controller {
 
     private static final Logger logger = LoggerFactory.getLogger(Controller.class);
 
-    public ArrayList<String> getFilesBatch ()
+    public Iterable<Result<Item>> getFilesBatch ()
     {
         /*
         Sends all the videos that are not yet converted to mpd version
@@ -42,19 +43,51 @@ public class Controller {
         Iterable<Result<Item>> results = minioClient.listObjects(
                 ListObjectsArgs.builder().bucket("videos").build());
 
-        System.out.println("bucket objects " + results);
 
-
-        return new ArrayList<>();
+        return results;
     }
 
     @GetMapping("/")
-    public HashMap<String, Object> home() throws IOException {
-        getFilesBatch();
-//        String input = "http://167.99.252.122:9000/videos/086363c1656dcc5391e81856df039a04.mp4";
-//        String outputLocation = "/home/nilarnab/Documents/video-preprocessing-system-spring-boot/src/dashVideos/" + "output.mpd";
-//        VideoProcessor videoProcessor = new VideoProcessor(input, outputLocation);
-//        boolean resp = videoProcessor.convert();
+    public HashMap<String, Object> home() throws IOException, ServerException, InsufficientDataException, ErrorResponseException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        String BASE_URL = "http://167.99.252.122:9000/videos/"; // hardcoded
+        String BASE_LOCAL_FILE_STORAGE = "/home/nilarnab/Documents/video-preprocessing-system-spring-boot/src/dashVideos/"; // hardcoded
+        Iterable<Result<Item>> convertibleVideosList = getFilesBatch();
+        for(Result<Item> videoMP4: convertibleVideosList)
+        {
+            String videoName = videoMP4.get().objectName().replaceAll(" ", "%20");
+            String videoUrl = BASE_URL + videoName;
+            String videoKey = videoName.split("\\.")[0];
+            String mpdVideoLocalBufferPath = BASE_LOCAL_FILE_STORAGE + videoKey;
+            File f1 = new File(mpdVideoLocalBufferPath);
+            if (f1.mkdir())
+            {
+                String outputLocation = mpdVideoLocalBufferPath + '/' + videoKey + ".mpd";
+                System.out.println(videoUrl);
+                System.out.println(outputLocation);
+
+                VideoProcessor videoProcessor = new VideoProcessor(videoUrl, outputLocation);
+                boolean resp = videoProcessor.convert();
+
+                if (resp)
+                {
+                    // ship the files
+
+                    // edit the .mpd file
+                    // ship the .mpd file in a folder
+                    // put all other files in the same folder
+                    // delete the directory
+                }
+                else
+                {
+                    System.out.println("Could not convert " + videoKey);
+                }
+            }
+            else
+            {
+                System.out.println("Could not make a folder for " + videoKey);
+            }
+        }
+
         return (new Response(true, "request complete")).getResponse();
     }
     @GetMapping("/convert")
